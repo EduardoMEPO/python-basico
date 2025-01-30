3550a5c7f31bb54d8e485a072944766d43085cca
Eduardo Mendez <eemp_26@hotmail.com>
2025-01-29 19:32:38 -0500

    ejerc-2a-2.py

---
+++ ejercicios-2a-2.py
@@ -0,0 +1,6 @@
+'''
+CONTRASEÑA SEGURA:
+Por motivos de seguridad una contraseña debe tener una vocal en minúscula, dos símbolos
+especiales diferentes (pueden ser solo * o #). Dada una contraseña ingresada por el usuario,
+comprueba si es una contraseña segura e indícalo por pantalla
+'''
