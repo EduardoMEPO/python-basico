d18452c354de127077504724c82a1533e471f5f0
Eduardo Mendez <eemp_26@hotmail.com>
2025-01-29 19:37:39 -0500

    Realizando el ejerc-2a-2

--- ejercicios-2a-2.py
+++ ejercicios-2a-2.py
@@ -4,3 +4,4 @@ Por motivos de seguridad una contraseña debe tener una vocal en minúscula, dos
 especiales diferentes (pueden ser solo * o #). Dada una contraseña ingresada por el usuario,
 comprueba si es una contraseña segura e indícalo por pantalla
 '''
+print("Hola")
