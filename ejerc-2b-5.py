a72da0f0d417634a74944c31a088a588a85af57f
Eduardo Mendez <eemp_26@hotmail.com>
2025-01-30 16:32:47 -0500

    ejerc-2b-5

---
+++ ejerc-2b-5.py
@@ -0,0 +1,15 @@
+'''
+MAYUSCULA O MINUSCULA (BONUS*):
+Permite que el usuario introduzca una letra (del alfabeto latino) como input. Comprueba si esta es
+una mayúscula o una minúscula.'''
+# Introducir una letra
+letra = input("Letra: ")
+letras_minusculas = "abcdefghijklmnñopqrstuvwxyz"
+letras_mayusculas = "ABCDEFGHIJKLMNOPQRSTUVXWYZ"
+# Comprobar si es mayúscula o minúscula
+if letra in letras_mayusculas:
+    print(f"La letra {letra} esta en mayúscula.")
+elif letra in letras_minusculas:
+    print(f"La letra {letra} esta en minúsculas.")
+else:
+    print("¡Usted no ingreso una letra.")
