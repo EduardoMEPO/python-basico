d5cff0e684746838b1ed0ec913908133d1226b47
Eduardo Mendez <eemp_26@hotmail.com>
2025-01-30 12:27:30 -0500

    ejerc-2a-2

--- ejercicios-2a-2.py
+++ ejercicios-2a-2.py
@@ -4,8 +4,14 @@ Por motivos de seguridad una contraseña debe tener una vocal en minúscula, dos
 especiales diferentes (pueden ser solo * o #). Dada una contraseña ingresada por el usuario,
 comprueba si es una contraseña segura e indícalo por pantalla
 '''
-# Pedir la contraseña al usuario
+# Ingreso de contraseña
 password = input("Password: ")
 
-password = password.replace(" ","")
-print(password)
+#1. La contraseña debe de tener:
+# 1 vocal en minúsculas y 2 simbolos especiales diferentes (* o #)
+if ("a" in password or "e" in password or "i" in password or "o" in password or "u" in password) and ("*" in password and "#" in password):
+    # Indicar por pantalla si es segura la contraseña
+    print("La contraseña es segura")
+else:
+    # Indicar por pantalla si no es segura la contraseña
+    print("La contraseña no cumple con los requisitos de seguridad")
