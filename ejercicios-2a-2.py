1e000cc3ffc61c7320dc9a55648775e492c1d808
Eduardo Mendez <eemp_26@hotmail.com>
2025-01-29 21:27:35 -0500

    ejerc-2a-2

--- ejercicios-2a-2.py
+++ ejercicios-2a-2.py
@@ -4,4 +4,6 @@ Por motivos de seguridad una contraseña debe tener una vocal en minúscula, dos
 especiales diferentes (pueden ser solo * o #). Dada una contraseña ingresada por el usuario,
 comprueba si es una contraseña segura e indícalo por pantalla
 '''
-print("Hola")
+contrasenia = input("Password: ")
+contrasenia = contrasenia.replace(" ","")
+print(contrasenia)
